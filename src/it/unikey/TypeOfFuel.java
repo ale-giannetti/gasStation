71d79da2eb00961532ad3a42786b9abd8029ec33
Yanik_Dimitrov <dimitrov.yanik96@yahoo.com>
2022-02-01 16:54:38 +0100

    Refueling method

---
+++ src/it/unikey/TypeOfFuel.java
@@ -0,0 +1,23 @@
+package it.unikey;
+
+public class TypeOfFuel extends GasStation {
+
+    private static double PriceGreen = 5;
+    private static double PriceDisel = 10;
+
+    @Override
+    public void Refueling(double quanity) {
+        double Maxcapacity = 2000;
+        double capacity = 0;
+
+        for (int i = 0; i < quanity; i++) {
+            capacity++;
+        }
+        if (quanity > Maxcapacity)
+            System.out.println("Non puoi rifornire questa quantità di benzina nel distributore");
+        else
+            System.out.println("Hai rifornito " + quanity);
+    }
+
+
+}
