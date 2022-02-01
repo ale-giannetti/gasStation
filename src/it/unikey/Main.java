22f1b6faa773750e3294e0baa591e92a0b0ec6b7
LAPTOP-AD1HIVPQ\ACER <alessio.giannetti.gnn@gmail.com>
2022-02-01 17:07:43 +0100

    Main

--- src/it/unikey/Main.java
+++ src/it/unikey/Main.java
@@ -3,6 +3,21 @@ package it.unikey;
 public class Main {
 
     public static void main(String[] args) {
+        
+        GasStation gs = new GasStation();
+        TypeOfFuel sg = new TypeOfFuel();
+
+        gs.Refueling(200);
+        gs.Refueling(200);
+        //gs.Refueling(700);
+
+        gs.Sell(50);
+        gs.UpdatePrice(2);
+        gs.Sell(50);
+
+        sg.Refueling(2500);
+        sg.SellFuelGreen("Mercedes", "Classe A", 70);
+        sg.SellDisel("Opel", "Corsa", 25);
 
     }
 }
